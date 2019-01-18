a816e4c827465c2c5b9ed2dada3218e8a4bcae49
Ren <45683120+renCoder1@users.noreply.github.com>
2019-01-17 18:44:12 -0800

    rollTheDice1.0
    Javascript functionality

---
+++ index.js
@@ -0,0 +1,16 @@
+alert("hello");
+
+function onRollClick() {
+  var randomDiceNumber1 = Math.floor(Math.random() * 6 + 1);
+  var randomDiceNumber2 = Math.floor(Math.random() * 6 + 1);
+  
+  var redDiceImg = randomDiceNumber1 + ".png";
+  var blackDiceImg = randomDiceNumber2 + ".png";
+
+  var redDice = document.getElementsByClassName('diceRed');
+  var blackDice = document.getElementsByClassName('diceBlack');
+
+  redDice[0].setAttribute("src", "images/" + redDiceImg);
+  blackDice[0].setAttribute("src", "images/black" + blackDiceImg);
+
+}
